d8a694736fbaeb7d62bcbbec5063fd68092aa7b8
DavidQ23 <davidqua23@gmail.com>
2021-05-30 13:18:58 +0200

    Update

--- Praktikumsaufgaben/Aufgabe3.1/Client/script.js
+++ Praktikumsaufgaben/Aufgabe3.1/Client/script.js
@@ -1,22 +1,22 @@
 "use strict";
 var P_3_1Server;
 (function (P_3_1Server) {
-    async function handleRequest() {
+    async function communication() {
         let formData = new FormData(document.forms[0]); // Auslesen der eingebenen Daten des ersten Formulars im Dokument
-        let url = "https://davidqgissose2021.herokuapp.com/";
-        let query = new URLSearchParams(formData); //Vorliegende Daten aus dem Formular werden aus einem FormData-Objekt generiert
-        url = url + "?" + query.toString(); //query String wird an URL hinten angehängt
-        console.log(url); //URL wird in Kosole ausgegeben
         for (let entry of formData) { //Konsolenausgabe der Key-Value-Paare des Formulars
             console.log(entry);
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
         }
+        let url = "https://davidqgissose2021.herokuapp.com/";
+        let query = new URLSearchParams(formData); //Vorliegende Daten aus dem Formular werden aus einem FormData-Objekt generiert
+        url = url + "?" + query.toString(); //query String wird an URL hinten angehängt
+        console.log(url); //URL wird in Kosole ausgegeben
         let response = await fetch(url); //Auf die URL warten zum Abschicken zum Server
         let answer = await response.text(); //Warten auf Antwort des Servers in Form eines Strings
         console.log(answer); //Konsolenausgabe der Serverantwort
     }
     let button = document.getElementById("SubmitButton");
-    button.addEventListener("click", handleRequest);
+    button.addEventListener("click", communication);
 })(P_3_1Server || (P_3_1Server = {}));
 //# sourceMappingURL=script.js.map
--- Praktikumsaufgaben/Aufgabe3.1/Client/script.ts
+++ Praktikumsaufgaben/Aufgabe3.1/Client/script.ts
@@ -1,10 +1,6 @@
 namespace P_3_1Server {
-    async function handleRequest(): Promise<void> {
+    async function communication(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);   // Auslesen der eingebenen Daten des ersten Formulars im Dokument
-        let url: string = "https://davidqgissose2021.herokuapp.com/";
-        let query: URLSearchParams = new URLSearchParams(<any>formData); //Vorliegende Daten aus dem Formular werden aus einem FormData-Objekt generiert
-        url = url + "?" + query.toString();                         //query String wird an URL hinten angehängt
-        console.log(url);                                           //URL wird in Kosole ausgegeben
 
         for (let entry of formData) {                               //Konsolenausgabe der Key-Value-Paare des Formulars
             console.log(entry);
@@ -12,11 +8,18 @@ namespace P_3_1Server {
             console.log("value: " + entry[1]);
         }
 
+        let url: string = "https://davidqgissose2021.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any>formData); //Vorliegende Daten aus dem Formular werden aus einem FormData-Objekt generiert
+        url = url + "?" + query.toString();                         //query String wird an URL hinten angehängt
+        console.log(url);                                           //URL wird in Kosole ausgegeben
+
+        
+
         let response: Response = await fetch(url);                  //Auf die URL warten zum Abschicken zum Server
         let answer: string = await response.text();                 //Warten auf Antwort des Servers in Form eines Strings
         console.log(answer);                                        //Konsolenausgabe der Serverantwort
 
     }
     let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("SubmitButton");
-    button.addEventListener("click", handleRequest);
+    button.addEventListener("click", communication);
 }
